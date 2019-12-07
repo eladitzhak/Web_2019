d73195ea086de89144e2e6131ae2e88905299960
elad itzhak <37639908+eladitzhak@users.noreply.github.com>
2019-12-08 01:50:00 +0200

    Add files via upload

--- Exercises/2/Handlers/postHandler.js
+++ Exercises/2/Handlers/postHandler.js
@@ -2,6 +2,16 @@ const Order = require('../Order');
 const data = require('../data');
 const moment = require('moment');
 
+// eslint-disable-next-line require-jsdoc
+function validatingJSON (json) {
+	let checkedjson = false;
+	try {
+		checkedjson = JSON.parse(json);
+	} catch (e) {
+
+	}
+	return checkedjson;
+}
 const insertNewData = (req, res) => {
 	let body = '';
 	// eslint-disable-next-line arrow-parens
@@ -9,24 +19,32 @@ const insertNewData = (req, res) => {
 		body += chunk.toString();
 	});
 	req.on('end', () => {
-		const newDataItem = JSON.parse(body);
-		if (data.totalTickets - newDataItem.numOfTickets < 0) {
-			const msg = `cannot create new order! not enough tickets to supply. requested ${newDataItem.numOfTickets} actual ${data.totalTickets}`;
+		// const newDataItem = JSON.parse(body);
+		const newDataItem = validatingJSON(body);
+		if (newDataItem == false) {
+			const msg = 'cannot create new order! missing data!';
 			console.log(msg);
 			res.writeHeader(400);
 			res.end(msg);
 		} else {
-			const newOrder = new Order(newDataItem.first_name, newDataItem.last_name, newDataItem.numOfTickets)
-				.on('orderChanged', () => console.log(`orderchnged: date: ${moment().format()} data: ${JSON.stringify(newOrder.getAllInfo())} `))
-				.on('orderChanged', () => data.logs.push({ ACTION: 'orderchnged', date: moment().format(), data: newOrder.getAllInfo() }));
-				// .on('orderCreated', () => data.logs.push({ ACTION: 'orderCreated', date: moment().format() }))
-			data.totalTickets -= newDataItem.numOfTickets;
-			data.orders.push(newOrder);
-			data.logs.push({ ACTION: 'orderCreated', date: moment().format(), data: newOrder.getAllInfo() });
-			// data.logs.push('OrderCreated', newOrder);
-			// console.log('data pushed succesfuly');
-			res.writeHeader(200);
-			res.end('New order created succeffuly ' + newOrder.getAllInfo());
+			if (data.totalTickets - newDataItem.numOfTickets < 0) {
+				const msg = `cannot create new order! not enough tickets to supply. requested ${newDataItem.numOfTickets} actual ${data.totalTickets}`;
+				console.log(msg);
+				res.writeHeader(400);
+				res.end(msg);
+			} else {
+				const newOrder = new Order(newDataItem.first_name, newDataItem.last_name, newDataItem.numOfTickets)
+					.on('orderChanged', () => console.log(`orderchnged: date: ${moment().format()} data: ${JSON.stringify(newOrder.getAllInfo())} `))
+					.on('orderChanged', () => data.logs.push({ ACTION: 'orderchnged', date: moment().format(), data: newOrder.getAllInfo() }));
+					// .on('orderCreated', () => data.logs.push({ ACTION: 'orderCreated', date: moment().format() }))
+				data.totalTickets -= newDataItem.numOfTickets;
+				data.orders.push(newOrder);
+				data.logs.push({ ACTION: 'orderCreated', date: moment().format(), data: newOrder.getAllInfo() });
+				// data.logs.push('OrderCreated', newOrder);
+				// console.log('data pushed succesfuly');
+				res.writeHeader(200);
+				res.end('New order created succeffuly ' + newOrder.getAllInfo());
+			}
 		}
 	});
 };
